b9c503f39bca86b74c1d658e879fd8134cbf4bac
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-01-19 15:09:10 -0300

    fim curso Obj

--- assets/ordenar.js
+++ assets/ordenar.js
@@ -16,3 +16,7 @@ function ordenar(lista, propriedade) {
 const ordenadoNome = ordenar(clientes, clientes);
 
 console.log(ordenadoNome);
+
+// Que podemos utilizar os métodos de listas em conjunto com objetos;
+// Como passamos os parâmetros e propriedades dos objetos para alguns métodos de listas;
+// Vimos exemplos de como encontrar, filtrar e ordenar uma lista de objetos.
